6be45963787826cbbc0440cec4e15481cc371671
Ilia Iliev <iliev@Ilias-MacBook-Pro.local>
2022-12-15 21:30:35 +0000

    Adding neccassery imports to booking Controller

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,6 +3,7 @@ const Tour = require('../models/tourModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
+const AppError = require('../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
